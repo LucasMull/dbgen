10b540d7b296aefc9a06cca84aefa456747d72e6
LucasMull <muller.lucas@Hotmail.com>
2020-04-18 20:31:53 -0300

    test: add password data to each client

--- main.c
+++ main.c
@@ -28,12 +28,13 @@ int main(void)
 
     FILE *f_name; //list of brazilian names
     FILE *f_surnam; //list of brazilian surnames
+    FILE *f_psw; //list of passwords
     FILE *f_out; //generated individuals
 
     size_t i;    
 
     t_HEAP HEAP;
-    t_BLOCK b_name, b_surnam, b_ID;
+    t_BLOCK b_name, b_surnam, b_psw, b_ID;
 
     if ( !locale ){
         fprintf(stderr,"Couldn't set locale");
@@ -44,18 +45,25 @@ int main(void)
     initBLOCK(&b_name, &HEAP); //init and assign b_name to HEAP.addr[0]
     initBLOCK(&b_surnam, &HEAP); //init and assign b_surnam to HEAP.addr[1]
     initBLOCK(&b_ID, &HEAP); //init and assign b_ID to HEAP.addr[2]
+    initBLOCK(&b_psw, &HEAP); //init and assign b_psw to HEAP.addr[3]
     
     f_name = fopen("content/nomes.txt", "r"); 
     f_surnam = fopen("content/sobrenomes.txt", "r"); 
+    f_psw = fopen("content/passwords.txt", "r"); 
+
     if ( !f_name )
         fprintf(stderr,"Couldn't open nomes.txt");
     if ( !f_surnam )
         fprintf(stderr,"Couldn't open sobrenomes.txt");
+    if ( !f_psw )
+        fprintf(stderr,"Couldn't open sobrenomes.txt");
   
-    fileToBLOCK(f_name, HEAP.addr[NAME]);
+    fileToBLOCK(f_name, HEAP.addr[NAME], 0);
     fclose(f_name);    
-    fileToBLOCK(f_surnam, HEAP.addr[SURNAME]);    
+    fileToBLOCK(f_surnam, HEAP.addr[SURNAME], 0);    
     fclose(f_surnam);
+    fileToBLOCK(f_psw, HEAP.addr[PSW], DBSIZE);    
+    fclose(f_psw);
     
     numsToHEAP(&b_ID, MIN_2, MAX_2, AMT_2, LENGTH_2);
 
@@ -78,7 +86,7 @@ void getUsers(FILE* f_out, t_BLOCK *addr[])
     char s_agency[AMT_3][LENGTH_3], s_account[AMT_4][LENGTH_4];
 
     char NIL = '\0';
-    char *child;
+    char *acc;
     size_t i, rand_i, rand_j;
     
     char folder[20];
@@ -109,14 +117,14 @@ void getUsers(FILE* f_out, t_BLOCK *addr[])
         do{
             rand_j = rand()%AMT_4;
             while ( rand_j < AMT_4 ){
-                if ( child = uniqueNodeData(T+rand_i,s_account[rand_j]) )
+                if ( acc = uniqueNodeData(T+rand_i,s_account[rand_j]) )
                     break;
                 ++rand_j;
             }
-        } while (!child);
-        insertNode(T+rand_i, initNode(new_node), child);
+        } while (!acc);
+        insertNode(T+rand_i,initNode(new_node),acc,pickRandom(addr[PSW]));
         subject.attribute_3 = s_agency[rand_i];
-        subject.attribute_4 = child;
+        subject.attribute_4 = acc;
          
         // PRINTS DATABASE TO OUTPUT STREAM 
         fprintf(f_out,"%s,%s,%s,%s %s %s\n",subject.attribute_2,
