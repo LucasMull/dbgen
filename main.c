e8640abbc061e0212da96c09248eb3ae0d7f7b79
LucasMull <muller.lucas@Hotmail.com>
2020-05-02 18:24:52 -0300

    refactor: change 'subject' struct to 'client' and change it's variable names to what its representing

--- main.c
+++ main.c
@@ -8,15 +8,12 @@
 #include <time.h>
 #include <assert.h>
 
-typedef struct subject {
-    char *attribute_0;
-    char *attribute_1[3]; //for surname, maximum of 3
-    char *attribute_2;
-    char *attribute_3;
-    char *attribute_4;
-    //int attribute_5;
-    //void *attribute_6;
-    // create or remove attributes as needed
+typedef struct client {
+    char *name;
+    char *surname[3]; //for surname, maximum of 3
+    char *id;
+    char *agency;
+    char *account;
 } t_subj;
 
 void getUsers(FILE* f_out, t_BLOCK *addr[]);
@@ -28,20 +25,20 @@ int main(void)
     char *locale = setlocale(LC_CTYPE, ""); //set locale to UTF8
 
     FILE *f_name; //list of brazilian names
-    FILE *f_surnam; //list of brazilian surnames
+    FILE *f_surname; //list of brazilian surnames
     FILE *f_psw; //list of passwords
     FILE *f_out; //generated individuals
 
     size_t i;    
 
     t_HEAP HEAP;
-    t_BLOCK b_name, b_surnam, b_psw, b_ID;
+    t_BLOCK b_name, b_surname, b_psw, b_ID;
 
     assert(locale);
     fprintf(stderr,"Locale is set to %s\n",locale);
     
     initBLOCK(&b_name, &HEAP); //init and assign b_name to HEAP.addr[0]
-    initBLOCK(&b_surnam, &HEAP); //init and assign b_surnam to HEAP.addr[1]
+    initBLOCK(&b_surname, &HEAP); //init and assign b_surname to HEAP.addr[1]
     initBLOCK(&b_ID, &HEAP); //init and assign b_ID to HEAP.addr[2]
     initBLOCK(&b_psw, &HEAP); //init and assign b_psw to HEAP.addr[3]
     
@@ -50,10 +47,10 @@ int main(void)
     fileToBLOCK(f_name, HEAP.addr[NAME], 0);
     fclose(f_name);    
 
-    f_surnam = fopen("content/sobrenomes.txt", "r"); 
-    assert(f_surnam);
-    fileToBLOCK(f_surnam, HEAP.addr[SURNAME], 0);    
-    fclose(f_surnam);
+    f_surname = fopen("content/sobrenomes.txt", "r"); 
+    assert(f_surname);
+    fileToBLOCK(f_surname, HEAP.addr[SURNAME], 0);    
+    fclose(f_surname);
 
     f_psw = fopen("content/passwords.txt", "r"); 
     assert(f_psw);
@@ -79,7 +76,7 @@ void getUsers(FILE* f_out, t_BLOCK *addr[])
 {
     t_tree T[AMT_3];
     t_node *new_node;
-    t_subj subject;
+    t_subj client;
     char s_agency[AMT_3][LENGTH_3], s_account[AMT_4][LENGTH_4];
 
     char NIL = '\0';
@@ -100,16 +97,16 @@ void getUsers(FILE* f_out, t_BLOCK *addr[])
     for ( i = 0 ; i < DBSIZE ; ++i ){
 
         // THIS CREATES ATTRIBUTE 0 AND 1
-        subject.attribute_0 = pickRandom(addr[NAME]);
-        subject.attribute_1[0] = pickRandom(addr[SURNAME]);
-        if ( strlen(subject.attribute_1[0]) > 3 ){
-            subject.attribute_1[1] = &NIL;
+        client.name = pickRandom(addr[NAME]);
+        client.surname[0] = pickRandom(addr[SURNAME]);
+        if ( strlen(client.surname[0]) > 3 ){
+            client.surname[1] = &NIL;
         } else {
-            subject.attribute_1[1] = pickRandom(addr[SURNAME]);
+            client.surname[1] = pickRandom(addr[SURNAME]);
         }
 
         // THIS CREATES ATTRIBUTE 2
-        subject.attribute_2 = pickIndex(addr[ID], i);
+        client.id = pickIndex(addr[ID], i);
 
         //THIS CREATES ATTRIBUTE 3 AND 4 (3 is a requisite for 4)
         temp_i = rand() % AMT_3;
@@ -119,19 +116,18 @@ void getUsers(FILE* f_out, t_BLOCK *addr[])
         //ARRUMAR j SOLTO QUE DEVERIA VIR DE exclToTree()
         insertNode( T+temp_i, initNode(new_node), acc, pickRandom(addr[PSW]) );
 
-        subject.attribute_3 = s_agency[temp_i];
-        subject.attribute_4 = acc;
+        client.agency = s_agency[temp_i];
+        client.account = acc;
         // PRINTS DATABASE TO OUTPUT STREAM 
-        fprintf(f_out,"%s,%s,%s,%s %s %s\n",subject.attribute_2,
-                                            subject.attribute_3, 
-                                            subject.attribute_4, 
-                                            subject.attribute_0,
-                                            subject.attribute_1[0],
-                                            subject.attribute_1[1]);
-        free(subject.attribute_2);
+        fprintf(f_out,"%s,%s,%s,%s %s %s\n",client.id,
+                                            client.agency, 
+                                            client.account, 
+                                            client.name,
+                                            client.surname[0],
+                                            client.surname[1]);
+        free(client.id); //since each pointer will only be used once
     }
     addr[ID]->size -= i;
-    assert( addr[ID]->size == 0 );
 
     system("rm -rf clients; mkdir -p clients");
     for ( i = 0 ;  i < AMT_3 ; ++i ){ //print every tree node
