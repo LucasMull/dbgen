4f2339b000c6a812958168dd309c615c083f23d1
LucasMull <muller.lucas@Hotmail.com>
2020-06-01 16:35:17 -0300

    refactor: add general library header files for easier compiling

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define _error(A,B) fprintf(stderr, "dbgen: %s%s\n", A, B)
+
+#define ERR_READ "READ ERROR -- "
+#define ERR_FILE "CANT READ FILE -- "
+#define ERR_LINK "CANT LINK MORE THAN TWO FILES"
+
+#endif
---
+++ dbgen.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+#include "src/parser.h"
+#include "src/start_col.h"
+#include "src/data_fetch.h"
+
+#endif
